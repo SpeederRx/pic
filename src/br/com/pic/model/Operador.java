7e8464a22ad035e4e059803ba348f44f34343e31
rodrigobsilva <rodrigobsilva@live.com>
2017-08-26 15:29:26 -0300

    Exclusao da Classe Pessoa - Inclusão de Atributos de Pessoa nas classes Socio e Operador

--- src/br/com/pic/model/Operador.java
+++ src/br/com/pic/model/Operador.java
@@ -1,29 +1,20 @@
 package br.com.pic.model;
 
-public class Operador extends Pessoa {
+import java.util.Date;
+
+public class Operador{
 	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private Date dataNasc;
+	private String email;
+	private Endereco endereco;
+	private String telefone;
 	private Long nivelAcesso;
 	private String senha;
 	private String descricao;
 	
-	public Long getNivelAcesso() {
-		return nivelAcesso;
-	}
-	public void setNivelAcesso(Long nivelAcesso) {
-		this.nivelAcesso = nivelAcesso;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
 	
-
+	
 }
--- src/br/com/pic/model/Socio.java
+++ src/br/com/pic/model/Socio.java
@@ -1,12 +1,62 @@
 package br.com.pic.model;
 
-public class Socio extends Pessoa {
+import java.util.Date;
+
+public class Socio {
 	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private Date dataNasc;
+	private String email;
+	private Endereco endereco;
+	private String telefone;
 	private String contaMovimento;
 	private String senha;
 	private  double classificacao; 
 	private TipoEnvio tipoEnvio;
-	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 	public String getContaMovimento() {
 		return contaMovimento;
 	}
@@ -32,5 +82,5 @@ public class Socio extends Pessoa {
 		this.tipoEnvio = tipoEnvio;
 	}
 	
-
+	
 }
