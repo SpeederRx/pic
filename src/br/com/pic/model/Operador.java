a4c84f2a11c4e8b99edb6968115ec8927b118c14
rodrigobsilva <rodrigobsilva@live.com>
2017-08-26 16:11:07 -0300

    Alteração dos atributos de Operador.java

--- src/br/com/pic/model/Operador.java
+++ src/br/com/pic/model/Operador.java
@@ -14,6 +14,66 @@ public class Operador{
 	private Long nivelAcesso;
 	private String senha;
 	private String descricao;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Long getNivelAcesso() {
+		return nivelAcesso;
+	}
+	public void setNivelAcesso(Long nivelAcesso) {
+		this.nivelAcesso = nivelAcesso;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 	
 	
 	
