b365e4ff17e1d34a08603ed158c555ccbc038a2a
paulassis <mpaula.assis@hotmail.com>
2017-08-26 19:09:30 -0300

    Inclusão de anotações para Entity - Atributos - nas classes Ativo, ContaPrincipal, Endereco e Lancamento

--- src/br/com/pic/model/Endereco.java
+++ src/br/com/pic/model/Endereco.java
@@ -1,15 +1,35 @@
 package br.com.pic.model;
 
+@Entity
+@Table(name = "endereco")
 public class Endereco {
 	
+	@Id
+	@Column(name = "ID")
 	private Long id;
+	
+	@Column(name = "RUA")
 	private String rua;
+	
+	@Column(name = "NUMERO")
 	private Integer numero;
+	
+	@Column(name = "COMPLEMENTO")
 	private String complemento;	
+	
+	@Column(name = "BAIRRO")
 	private String bairro;
+	
+	@Column(name = "CIDADE")
 	private String cidade;
+	
+	@Column(name = "ESTADO")
 	private String estado;
+	
+	@Column(name = "CEP")
 	private String cep;
+	
+	@Column(name = "PAIS")
 	private String pais;
 	
 	public Long getId() {
--- src/br/com/pic/model/Lancamento.java
+++ src/br/com/pic/model/Lancamento.java
@@ -1,78 +1,141 @@
 package br.com.pic.model;
 
+@Entity
+@Table(name = "lancamento")
 public class Lancamento {
 	
+	@Id
+	@Column(name = "ID")
 	private Long id;
+	
+	@ManyToOne(mappedBy="lancamentos")
+	@Column(name = "ID_CONTA_PRINCIPAL")
 	private ContaPrincipal contaPrincipal;
+	
+	@Column(name = "ID_ATIVO")
 	private Ativo ativo;
+	
+	@ManyToOne(mappedBy="lancamentos")
+	@Column(name = "ID_PROPOSTA")
 	private Proposta proposta;
+	
+	@Column(name = "VALOR_TOTAL_LANC")
 	private Double valorTotalLanc;
+	
+	@Column(name = "DESCRICAO")
 	private String descricao;
+	
+	@Column(name = "QTDE_LANCADA")
 	private Integer qtdeLancada;
-	private TipoLancamento tipoLancamento;
+	
+	@Column(name = "TIPO_LANCAMENTO")
+	private Integer tipoLancamento;
+	
+	@Column(name = "ID_OPERADOR")
 	private Operador operador;
-	private EstadoLancamento estadoLancamento;
+	
+	@Column(name = "ESTADO_LANCAMENTO")
+	private Integer estadoLancamento;
+
+
 	public Long getId() {
 		return id;
 	}
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 	public ContaPrincipal getContaPrincipal() {
 		return contaPrincipal;
 	}
+
+
 	public void setContaPrincipal(ContaPrincipal contaPrincipal) {
 		this.contaPrincipal = contaPrincipal;
 	}
+
+
 	public Ativo getAtivo() {
 		return ativo;
 	}
+
+
 	public void setAtivo(Ativo ativo) {
 		this.ativo = ativo;
 	}
+
+
 	public Proposta getProposta() {
 		return proposta;
 	}
+
+
 	public void setProposta(Proposta proposta) {
 		this.proposta = proposta;
 	}
+
+
 	public Double getValorTotalLanc() {
 		return valorTotalLanc;
 	}
+
+
 	public void setValorTotalLanc(Double valorTotalLanc) {
 		this.valorTotalLanc = valorTotalLanc;
 	}
+
+
 	public String getDescricao() {
 		return descricao;
 	}
+
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
+
 	public Integer getQtdeLancada() {
 		return qtdeLancada;
 	}
+
+
 	public void setQtdeLancada(Integer qtdeLancada) {
 		this.qtdeLancada = qtdeLancada;
 	}
-	public TipoLancamento getTipoLancamento() {
+
+
+	public Integer getTipoLancamento() {
 		return tipoLancamento;
 	}
-	public void setTipoLancamento(TipoLancamento tipoLancamento) {
+
+
+	public void setTipoLancamento(Integer tipoLancamento) {
 		this.tipoLancamento = tipoLancamento;
 	}
+
+
 	public Operador getOperador() {
 		return operador;
 	}
+
+
 	public void setOperador(Operador operador) {
 		this.operador = operador;
 	}
-	public EstadoLancamento getEstadoLancamento() {
+
+
+	public Integer getEstadoLancamento() {
 		return estadoLancamento;
 	}
-	public void setEstadoLancamento(EstadoLancamento estadoLancamento) {
+
+
+	public void setEstadoLancamento(Integer estadoLancamento) {
 		this.estadoLancamento = estadoLancamento;
 	}
 	
-	
-		
+			
 }
